f955e7c6c8c9febd973bb046babde0796bdfaad8
Gitterhubbajabba <arjunanmalikha45@gmail.com>
2025-05-02 03:38:58 +0800

    Update README.md with new information

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -17,22 +17,24 @@ const Header = () => {
 
   return (
     <div className={`top-banner ${scrollPosition > 10 ? 'fade-out' : ''}`}>
-      <div className="banner-section banner-left clickable">
-        <div className="banner-text name">MALIKHA ARJUNAN</div>
-        <div className="banner-text id">RA2211027010254</div>
-      </div>
-      
-      <div className="banner-section banner-center clickable">
-        <div className="banner-text name"></div>
-        <div className="banner-text id"></div>
-      </div>
-      
-      <Link to="/members" style={{ textDecoration: 'none' }}>
-        <div className="banner-section banner-right clickable">
-          <div className="banner-text name"></div>
-          <div className="banner-text id"></div>
+      <div className="banner-container">
+        <div className="banner-section banner-left">
+          <div className="banner-text name">MALIKHA ARJUNAN</div>
+          <div className="banner-text id">RA2211027010254</div>
         </div>
-      </Link>
+        
+        <div className="banner-section banner-center">
+          <div className="banner-text name">KANAK SRIVASTA</div>
+          <div className="banner-text id">RA2211027010230</div>
+        </div>
+        
+        <Link to="/members" style={{ textDecoration: 'none', width: '33.33%' }}>
+          <div className="banner-section banner-right">
+            <div className="banner-text view-text">VIEW</div>
+            <div className="banner-text view-text">MEMBERS</div>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
